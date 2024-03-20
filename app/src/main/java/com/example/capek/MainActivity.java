e4e3d29e1a305327c9d1f290a265a694a45fcf6d
Ikhsanssss <ikhsanmaulanaoke14@gmail.com>
2024-03-20 20:38:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/capek/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.capek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ikhsan_maulana);
+    }
+}
